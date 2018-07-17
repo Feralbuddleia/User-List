940a652ad3033ba4627bab649e34e8237afe1362
Xiaoyan Liao <running923@gmail.com>
2018-07-17 10:11:35 -0700

    feature: add store provider

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
+import store from './store';
+import { Provider } from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+  document.getElementById('root'));
 registerServiceWorker();
