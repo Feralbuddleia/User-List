f6d09c5c8f0195615e384540aa2aad21f5cf4d68
Xiaoyan Liao <running923@gmail.com>
2018-07-17 10:11:53 -0700

    feature: add Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Router from './components/Router';
+import { BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Router />
+        </div>
+      </BrowserRouter>
     );
   }
 }
