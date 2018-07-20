a955155a2abebc77354ba1b7fe541cedca6ea655
Xiaoyan Liao <running923@gmail.com>
2018-07-20 11:28:24 -0700

    feature: add table title sort component

---
+++ client/src/containers/Title.js
@@ -0,0 +1,23 @@
+import { connect } from "react-redux";
+import React from "react";
+import { setSort } from "../actions/sort";
+import { getUserData } from "../actions/user";
+
+const Title = ({ doSort, children }) => (
+  <a href="#" onClick={doSort}>
+    {children}
+  </a>
+);
+
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  doSort: () => {
+    dispatch(setSort(ownProps.search));
+    dispatch(getUserData());
+    return false;
+  }
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(Title);
