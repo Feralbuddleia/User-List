c13fc73ccd5ee65f42fec359bc485d68ced9c6e5
Xiaoyan Liao <running923@gmail.com>
2018-07-20 11:42:32 -0700

    feature: add login router

--- client/src/components/Router.js
+++ client/src/components/Router.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import AddUser from '../containers/AddUser';
 import EditUser from '../containers/EditUser';
-import VisibleUserList from '../containers/VisibleUserList';
+import Dashboard from '../containers/Dashboard';
+import Login from '../containers/Login';
 
 const Router = () => (
   <Switch>
-    <Route exact path="/users" component={VisibleUserList} />
+    <Route exact path="/" component={Login} />
+    <Route exact path="/users" component={Dashboard} />
     <Route path="/users/add" component={AddUser}/>
     <Route path="/users/edit/:login" component={EditUser} />
   </Switch>
